02e74694bfa5b1de158c6775d635bf4f00f7f94b
koppalakumar <koppalakumar564@gmail.com>
2025-06-26 12:30:48 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAVPEYWEFKXPADMFPB"
-secret_key = "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"
+access_key = var.access_key
+secret_key = var.secret_key
 }
